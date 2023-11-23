871130243e6219c65653da659dd41ad53d798196
Luke Schumacher <luke.schumacher777@gmail.com>
2023-11-23 12:15:09 +0100

    Created Simple Decoder for Model & Plotly Heatmap for Results
    Heatmap is mainly for presentation and ITT Summit

---
+++ decode/Simple_decoder.py
@@ -0,0 +1,55 @@
+import csv
+
+# Encoded data
+encoded_data = [
+ [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+# Sequence mapping
+sequence_mapping = [
+    ["Seq_%SiemensSeq%\ AALScout"], ["Seq_%SiemensSeq%\ BEAT"], ["Seq_%SiemensSeq%\ BEAT_FQ"], #type: ignore
+    ["Seq_%SiemensSeq%\ BEAT_map"], ["Seq_%SiemensSeq%\ CV_nav"], ["Seq_%SiemensSeq%\ dess"], #type: ignore
+    ["Seq_%SiemensSeq%\ ep2d_diff"], ["Seq_%SiemensSeq%\ ep2d_fid"], ["Seq_%SiemensSeq%\ fast_tse"], #type: ignore
+    ["Seq_%SiemensSeq%\ fl3d_ce"], ["Seq_%SiemensSeq%\ fl3d_vibe"], ["Seq_%SiemensSeq%\ gre"], #type: ignore
+    ["Seq_%SiemensSeq%\ gre_field_mapping"], ["Seq_%SiemensSeq%\ haste"], ["Seq_%SiemensSeq%\ qDWI"], #type: ignore
+    ["Seq_%SiemensSeq%\ resolve"], ["Seq_%SiemensSeq%\ space"], ["Seq_%SiemensSeq%\ svs_se"], #type: ignore
+    ["Seq_%SiemensSeq%\ svs_st"], ["Seq_%SiemensSeq%\ tfl"], ["Seq_%SiemensSeq%\ tfl_b1map"], #type: ignore
+    ["Seq_%SiemensSeq%\ tfl_cb"], ["Seq_%SiemensSeq%\ trufi"], ["Seq_%SiemensSeq%\ tse"], ["Seq_%SiemensSeq%\ tse_dixon"] #type: ignore
+]
+
+# Decode the data
+decoded_data = [sequence_mapping[i][0] for i, sequence in enumerate(encoded_data) if 1 in sequence]
+
+# Output to CSV with quotechar to handle special characters
+output_csv = "model_results/head_75609.csv"
+with open(output_csv, mode='w', newline='') as file:
+    writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["Sequence"])
+    for sequence in decoded_data:
+        writer.writerow([sequence])
+
+print(f"Decoded data has been saved to {output_csv}")

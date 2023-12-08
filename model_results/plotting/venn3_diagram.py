f82a2f4fdc44e815fdf2f74e7e319ecbf17130cc
Luke Schumacher <luke.schumacher777@gmail.com>
2023-12-08 10:43:08 +0100

    Copy of Model with duration

--- model_results/plotting/venn3_diagram.py
+++ model_results/plotting/venn3_diagram.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 
 # Your data
-knee_175608 = set(["Seq_gre", "Seq_gre", "Seq_tse", "Seq_tse", "Seq_tse", "Seq_tse", "Seq_tse", "Seq_tse"])
-knee_182627 = set(["Seq_AALScout", "Seq_tse", "Seq_tse_dixon", "Seq_tse_dixon", "Seq_tse", "Seq_fl3d_vibe", "Seq_tse_dixon", "Seq_fl3d_vibe", "Seq_haste"])
-knee_202531 = set(["Seq_AALScout", "Seq_tse", "Seq_space", "Seq_tse", "Seq_tse", "Seq_tse", "Seq_tse"])
+knee_175608 = set(["gre", "gre", "tse", "tse", "tse", "tse", "tse", "tse"])
+knee_182627 = set(["AALScout", "tse", "tse_dixon", "tse_dixon", "tse", "fl3d_vibe", "tse_dixon", "fl3d_vibe", "haste"])
+knee_202531 = set(["AALScout", "tse", "space", "tse", "tse", "tse", "tse"])
 
 # Create the Venn diagram
 venn_labels = {'100': 'Knee 175608', '010': 'Knee 182627', '110': 'Common (Knee 175608 and Knee 182627)',
@@ -20,12 +20,12 @@ def subset_label_formatter(x):
 venn3(subsets=(len(knee_175608), len(knee_182627), len(knee_175608.intersection(knee_182627)),
                len(knee_202531), len(knee_175608.intersection(knee_202531)), len(knee_182627.intersection(knee_202531)),
                len(knee_175608.intersection(knee_182627).intersection(knee_202531))),
-      set_labels=('Knee 175608', 'Knee 182627', 'Knee 202531'), set_colors=('skyblue', 'lightgreen', 'lightcoral'),
+      set_labels=('Knee 175608\n' + str(len(knee_175608)), 'Knee 182627\n' + str(len(knee_182627)), 'Knee 202531\n' + str(len(knee_202531))),
+      set_colors=('skyblue', 'lightgreen', 'lightcoral'),
       alpha=0.7, ax=plt.gca(), subset_label_formatter=subset_label_formatter)
 
-
 # Title
-plt.title('Venn Diagram of Knee Sequences Across 3 Customers')
+plt.title('Venn Diagram of Knee Sequences across 3 Customers')
 
 # Show the plot
 plt.show()
